e424ffcee4719114b060d8ad7b84ff1f14dfcefc
Kannan Padri <kannan.padr@outlook.com>
2026-06-08 19:25:17 +0530

    mobile number unqiue for registration

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  phone: { type: String },
+  phone: { type: String, required: true, unique: true },
   pinHash: { type: String },
   role: { type: String, enum: ['admin', 'user'], default: 'user' },
   createdAt: { type: Date, default: Date.now }
